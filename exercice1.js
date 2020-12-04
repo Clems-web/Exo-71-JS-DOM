ed51e97696608fdb3a7e7f9b38627f27e6b38b61
Clems-web <clement.servais1@gmail.com>
2020-12-04 09:37:05 +0100

    Finito

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,17 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
 
 document.getElementById('viewport').appendChild(monDiv);
+
+let mySecondDiv = document.createElement("div");
+document.getElementById('viewport').appendChild(mySecondDiv);
+mySecondDiv.innerHTML = "blabla";
+mySecondDiv.style.backgroundColor = "red";
+mySecondDiv.style.width = 100 + "px";
+mySecondDiv.style.height = 100 + "px";
+
+document.getElementById("aSupprimer").remove();
+
 
 
 
